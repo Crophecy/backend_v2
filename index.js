18b26a9c07b26a17692e8ce73553a0eb384f28f6
rishabhsssharma <manyrishabh@gmail.com>
2024-03-06 14:15:22 +0530

    configured_signup

---
+++ index.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const userRoute = require("./controller/userRoute");
+const app = express();
+
+mongoose.set("strictQuery",true);
+mongoose.connect("mongodb+srv://manyrishabh:hack123@cluster0.r8nobs1.mongodb.net/");
+var db = mongoose.connection;
+db.on("open",()=>console.log("Connected to DB"));
+db.on("error",()=>console.log("Error occurred"));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
+
+app.use("/auth", userRoute);
+
+
+app.listen(4000,()=>{
+    console.log("Server connected at 4000");
+})

265c794c26a1513a1a25c6e525350045ab9b848f
Abhijeet Pundkar <98893864+AbhiPundkar@users.noreply.github.com>
2024-03-07 14:24:10 +0530

    nodemailer,about_page

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const userRoute = require("./controller/userRoute");
 const app = express();
+const nodemailer = require('nodemailer');
 
 mongoose.set("strictQuery",true);
 mongoose.connect("mongodb+srv://manyrishabh:hack123@cluster0.r8nobs1.mongodb.net/");
@@ -16,7 +17,34 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 
 app.use("/auth", userRoute);
-
+app.post('/contact', async (req, res) => {
+    let { name, email,contactNumber, query } = req.body;
+  
+    let transport = nodemailer.createTransport({
+      host: "sandbox.smtp.mailtrap.io",
+      port: 2525,
+      auth: {
+        user: "7bc7663e0c74dc",
+        pass: "ff961f48ffd607"
+      }
+    });
+  
+    let mailOptions = {
+      from: 'abhi021102@gmail.com',
+      to: 'abhi021102@gmail.com',
+      subject: `Message from ${name}`,
+      text: `From: ${email}\n\n${query}\n\n ${contactNumber}`
+    };
+  
+    try {
+      await transport.sendMail(mailOptions);
+      res.status(200).send('Email sent');
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Error sending email');
+    }
+  });
+  
 
 app.listen(4000,()=>{
     console.log("Server connected at 4000");

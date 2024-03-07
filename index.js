f5ea9210419b22752d4bc0fe7af7817c1ba3e737
Abhijeet Pundkar <98893864+AbhiPundkar@users.noreply.github.com>
2024-03-07 14:49:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -44,6 +44,34 @@ app.post('/contact', async (req, res) => {
       res.status(500).send('Error sending email');
     }
   });
+
+app.post('/contact', async (req, res) => {
+    let { name, email, contactNumber, query } = req.body;
+  
+    let transport = nodemailer.createTransport({
+      host: "sandbox.smtp.mailtrap.io",
+      port: 2525,
+      auth: {
+        user: "7bc7663e0c74dc",
+        pass: "ff961f48ffd607"
+      }
+    });
+  
+    let mailOptions = {
+      from: `abhi021102@gmail.com`,
+      to: `abhi021102@gmail.com`,
+      subject: `Message from ${name}`,
+      text: `From: ${email}\n\n${query}\n\n ${contactNumber}`
+    };
+  
+    try {
+      await transport.sendMail(mailOptions);
+      res.status(200).json({message: 'Email sent'}); // Send JSON response
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({message: 'Error sending email'}); // Send JSON response
+    }
+});
   
 
 app.listen(4000,()=>{
